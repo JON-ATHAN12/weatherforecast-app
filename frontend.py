bac6426c0481fcb1e8e5a08750c3a857ddc7e9c0
Dell <jonathanphy1210@gmail.com>
2024-10-10 12:50:56 +0530

    Day 32: Edit temperature value, Error handling.

---
+++ frontend.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import plotly.express as px
+from backend import get_data
+
+st.title("Weather Forecast for the Next Days")
+place = st.text_input(label="Place:")
+days = st.slider(label="Forecast Days", min_value=1, max_value=5,
+                 help="select the number of forecasted days")
+option = st.selectbox(label="Select data to view", options=('Temperature', 'Sky'))
+st.subheader(f"{option} for the next {days} in {place}")
+
+if place:
+    # Getting the data:
+    try:
+        filtered_data = get_data(place, days)
+
+        if option == "Temperature":
+            temperatures = [dict['main']['temp'] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={'x': "Dates", 'y': "Temperature (C)"})
+            st.plotly_chart(figure)
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.markdown(':red[Check the place name]')
+
+
+
